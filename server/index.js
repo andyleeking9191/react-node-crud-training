7180afa8fa8fc50dcf38710166d310f7293af93e
Andrew King <andyleeking9191@yahoo.com>
2022-01-27 20:40:06 -0500

    connected the database to node and implemented test query

--- server/index.js
+++ server/index.js
@@ -1,6 +1,32 @@
+require('dotenv').config()
+const { response } = require('express');
 const express = require('express');
 const app = express();
 const port = 3001;
+
+
+
+const knex = require('knex')({
+    client: 'pg',
+    version: '12.9',
+    connection: {
+      host : `${process.env.HOST}`,
+      port : `${process.env.PORT}`,
+      user : `${process.env.DATABASE_USER}`,
+      password : `${process.env.DATABASE_PASSWORD}`,
+      database : `${process.env.DATABASE}`
+    }
+  });
+
+
+
+let movies = [];
+
+app.get('/users', (req, res) => {
+  knex.select('*').from('movie_review').then(data => {
+    res.send(data)
+  })
+})
 
 app.listen(port, ()=> {
     console.log(`The server is listening on port ${port}...`)
